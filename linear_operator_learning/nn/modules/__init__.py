d510172d6c4945481ce4e237ddff04d5677693b6
alekfrohlich <alek.frohlich@gmail.com>
2025-02-11 17:50:47 +0100

    Refactor 'detecting_independence.ipynb' to not use lightning nor fancy abstractions

--- linear_operator_learning/nn/modules/__init__.py
+++ linear_operator_learning/nn/modules/__init__.py
@@ -1,7 +1,6 @@
 """Modules entry point."""
 
-from .bilinear_net import BiLinearNet
 from .loss import L2ContrastiveLoss
 from .mlp import MLP
 
-__all__ = ["L2ContrastiveLoss", "MLP", "BiLinearNet"]
+__all__ = ["L2ContrastiveLoss", "MLP"]
